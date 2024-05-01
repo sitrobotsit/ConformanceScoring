94295f055b7eddfd4796ed12cc606fee5b5c5637
Luke Wilson <lukeuiwilson@gmail.com>
2024-05-01 14:09:55 +0200

    unbroken index

--- conformance-scoring/index.php
+++ conformance-scoring/index.php
@@ -33,14 +33,18 @@
         <div class="nsw-layout">
             <main class="nsw-layout__main">
                 <h1>Product Accessibility List (PAL)</h1>
-               <p>Date products recorded: 1 May 2024</p>
+                <p>Date products recorded: 1 May 2024</p>
                 <ul>
                     <li><a href="https://ozewai.org/resources/about-the-ozewai-product-accessibility-list-pal/">Read
-                            about the Product Accessibility List (PAL)</a> and how  products are scored</li>
-                    <li>Complete the <a href="https://forms.gle/ioYRFMNWAyrHwmoi6">Send a product form</a> to share products
+                            about the Product Accessibility List (PAL)</a> and how products are scored</li>
+                    <li>Complete the <a href="https://forms.gle/ioYRFMNWAyrHwmoi6">Send a product form</a> to share
+                        products
                         for OZeWAI to score and add to the PAL.</li>
-                    <li><a href="https://ozewai.org/resources/about-the-ozewai-product-accessibility-list-pal#PAL-feedback">Provide feedback about the OZeWAI PAL</a>, how we can improve it and how vendors can update their information.</li>
-                    
+                    <li><a
+                            href="https://ozewai.org/resources/about-the-ozewai-product-accessibility-list-pal#PAL-feedback">Provide
+                            feedback about the OZeWAI PAL</a>, how we can improve it and how vendors can update their
+                        information.</li>
+
                 </ul>
 
                 <div class="nsw-form">
@@ -73,16 +77,35 @@
                     <table id="dataTable">
                         <thead>
                             <tr>
+                                <!-- Row 1 -->
                                 <th>Vendor</th>
+
+                                <!-- Row 2 -->
                                 <th>Product</th>
+
+                                <!-- Row 3 -->
                                 <th>PAL Risk Rating</th>
+
+                                <!-- Row 4 -->
                                 <th>Category</th>
+
+                                <!-- Row 5 -->
                                 <th>Accessible Percent</th>
+
+                                <!-- Row 6 -->
                                 <th>WCAG A</th>
+
+                                <!-- Row 7 -->
                                 <th>WCAG AA</th>
+
+                                <!-- Row 8 -->
                                 <th>Accessibility Statement</th>
+
+                                <!-- Row 9 -->
                                 <th>Accessibility Conformance Report (ACR)</th>
-                                <th>VPAT details</th>
+
+                                <!-- Row 10 -->
+                                <!-- <th>VPAT details</th> -->
 
 
                             </tr>
@@ -94,42 +117,52 @@
 
                             foreach ($data as $row) {
                                 echo "<tr>";
-                                echo "<td><a href='" . htmlspecialchars($row['Vendor URL']) . "'>" . htmlspecialchars($row['Vendor']) . "</a>";
 
+                                // ROW 1
+                                echo "<td><a href='" . htmlspecialchars($row['Vendor URL']) . "'>" . htmlspecialchars($row['Vendor']) . "</a></td>";
+
+                                // ROW 2
                                 // Check if Product URL has data
                                 if ($row['Product URL']) {
                                     echo "<td><a href='" . htmlspecialchars($row['Product URL']) . "'>" .
-                                        htmlspecialchars($row['Product']) . "</a>";
+                                        htmlspecialchars($row['Product']) . "</a></td>";
                                 } else {
                                     echo "<td>" . htmlspecialchars($row['Product']) . "</td>";
                                 }
 
+                                // ROW 3
                                 echo "<td>" . htmlspecialchars($row['PAL Risk Rating']) . "</td>";
+
+                                // ROW 4
                                 echo "<td>" . htmlspecialchars($row['Category']) . "</td>";
 
+                                // ROW 5
                                 echo "<td>" . htmlspecialchars($row['Accessible Percent'] * 100) . "%</td>";
 
-
+                                // ROW 6
                                 echo "<td>" . htmlspecialchars($row['WCAG A']) . "</td>";
+
+                                // ROW 7
                                 echo "<td>" . htmlspecialchars($row['WCAG AA']) . "</td>";
 
-
+                                // ROW 8
                                 // Check if Accessibility Statement data starts with "http"
                                 if (strpos($row['Accessibility Statement'], 'http') === 0) {
-                                    echo "<td><a href='" . htmlspecialchars($row['Accessibility Statement']) . "'>" . htmlspecialchars($row['Product']) . " Accessibility Statement</a>";
+                                    echo "<td><a href='" . htmlspecialchars($row['Accessibility Statement']) . "'>" . htmlspecialchars($row['Product']) . " Accessibility Statement</a></td>";
                                 } else {
                                     echo "<td>" . htmlspecialchars($row['Accessibility Statement']) . "</td>";
                                 }
 
-
-
+                                // ROW 9
                                 // Check if ACR data starts with "http"
                                 if (strpos($row['Accessibility Conformance Report (ACR)'], 'http') === 0) {
-                                    echo "<td><a href='" . htmlspecialchars($row['Accessibility Conformance Report (ACR)']) . "'>" . htmlspecialchars($row['Product']) . " Accessibility Conformance Report (ACR)</a>";
+                                    echo "<td><a href='" . htmlspecialchars($row['Accessibility Conformance Report (ACR)']) . "'>" . htmlspecialchars($row['Product']) . " Accessibility Conformance Report (ACR)</a></td>";
                                 } else {
                                     echo "<td>" . htmlspecialchars($row['Accessibility Conformance Report (ACR)']) . "</td>";
                                 }
-                             echo "<td>" . htmlspecialchars($row['VPAT details']) . "</td>";
+
+                                // ROW 10
+                                // echo "<td>" . htmlspecialchars($row['VPAT details']) . "</td>";
                                 echo "</tr>";
                             }
                             ?>
